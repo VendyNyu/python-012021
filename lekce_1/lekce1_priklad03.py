a1c821736b8f81e72a646551ee02218e57b99bf1
VendyNyu <ven.bil@seznam.cz>
2021-02-19 19:00:25 +0100

    Domaci ukol_lekce1_priklad02

---
+++ lekce_1/lekce1_priklad03.py
@@ -0,0 +1,39 @@
+#Zasedačky
+
+hodina = int(input("V kolik hodin chcete zamluvit meeting room? "))
+
+volnePokoje = {
+  9: ["Amadeus", "Goya", "Vlasy"],
+  10: ["Forman", "Goya"],
+  11: [],
+  12: ["Amadeus", "Vlasy"]
+}
+
+
+if hodina in volnePokoje:
+    print(f"Počet volných místností: {len(volnePokoje[hodina])}")
+    print(f"Volné jsou tyto místnosti: {volnePokoje[hodina]}")
+else:
+    print("Žádná místnost není v této době k dispozici.")
+
+
+
+
+"""    
+#Počet volných místností:
+print(f"Počet volných místností: {len(mistnosti)}")
+
+
+# Seznam místností, které jsou volné:
+print(f"Volné jsou tyto místnosti: {mistnosti}")
+"""
+
+
+
+
+
+
+
+
+
+
