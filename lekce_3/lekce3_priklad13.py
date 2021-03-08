d43efa391d49b4a4e1527b6ccd2e035b00461222
VendyNyu <ven.bil@seznam.cz>
2021-03-08 18:32:29 +0100

    lekce3_kontrola prikladu 13

--- lekce_3/lekce3_priklad13.py
+++ lekce_3/lekce3_priklad13.py
@@ -1,7 +1,3 @@
-# Vrácení auta
-
-#Půjčení auta
-
 class Auto:
     def pujc_auto(self):
         if self.vypujcenost:
@@ -13,9 +9,9 @@ class Auto:
     def get_info(self):
         return f"Informace k autu: Značka a typ vozidla - {self.znacka_typvozidla}, registrační značka -  {self.registracni_znacka}."
 
-    def vrat_auto(self):
-        self.tachometr = self.kilometry + uzivatelKilometry
-        self.dny = uzivatelPocetDnu
+    def vrat_auto(self, tachometr, dny):
+        self.tachometr = self.tachometr + tachometr
+        self.dny = dny
         if self.dny == 0:
             price = 0
             cenaCelkem = self.dny * price
@@ -32,10 +28,10 @@ class Auto:
             self.vypujcenost = True
             return (f"Stav tachometru po vrácení je {self.tachometr} km. Cena za půjčení je {cenaCelkem} Kč.")
 
-    def __init__(self, registracni_znacka,znacka_typvozidla, kilometry):
+    def __init__(self, registracni_znacka,znacka_typvozidla, tachometr):
         self.registracni_znacka = registracni_znacka
         self.znacka_typvozidla = znacka_typvozidla
-        self.kilometry = int(kilometry)
+        self.tachometr = int(tachometr)
         self.vypujcenost = True
 
 
@@ -56,7 +52,6 @@ else:
     print("Auto není v naší půjčovně k dispozici.")
 """
 #Test, že funkce pujc_auto nedovolí půjčit stejné auto dvakrát:
-
 uzivatel = input("Zadejte značku auta, kterou si chcete půjčit: ")
 if uzivatel == "Peugeot":
     print(Auto1.get_info())
@@ -72,9 +67,9 @@ else:
 uzivatelPocetDnu = int(input("Zadej, jak dlouho měl zákazník auto k dispozici: "))
 uzivatelKilometry = int(input("Zadej počet ujetých kilometrů vozidla: "))
 if uzivatel == "Peugeot":
-    print(Auto1.vrat_auto())
+    print(Auto1.vrat_auto(uzivatelKilometry,uzivatelPocetDnu))
 elif uzivatel == "Škoda":
-    print(Auto2.vrat_auto())
+    print(Auto2.vrat_auto(uzivatelKilometry,uzivatelPocetDnu))
 else:
     print("Chyba. Špatně zadané paramtery.")
 
@@ -90,7 +85,3 @@ elif uzivatel == "Škoda":
 else:
     print("Auto není v naší půjčovně k dispozici.")
 """
-
-
-
-
