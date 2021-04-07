1052b11819eff36e6bd5e9909172703e55cc48f5
VendyNyu <ven.bil@seznam.cz>
2021-04-07 19:23:40 +0200

    kontrola_prikladu_30

--- lekce_6/lekce06_priklad30.py
+++ lekce_6/lekce06_priklad30.py
@@ -6,7 +6,7 @@ import openpyxl
 vykazy = pandas.read_csv('vykazy.csv')
 serazeni_projekty = vykazy.groupby('project')['hours'].sum()
 #pozor excel přípona xlsx
-#vykazy_excel = serazeni_projekty.to_excel('vykazy.xlsx')
+vykazy_excel = serazeni_projekty.to_excel('vykazy.xlsx')
 
 nacteni_excel = pandas.read_excel('vykazy.xlsx')
 print(nacteni_excel)
